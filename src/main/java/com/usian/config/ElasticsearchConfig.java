637994b4055d230d16a5813f9d022fec54f11a4f
lxz-0524 <316563774@qq.com>
2020-06-02 23:13:24 +0800

    增加了测试创建索引库、删除索引库的方法

--- src/main/java/com/usian/config/ElasticsearchConfig.java
+++ src/main/java/com/usian/config/ElasticsearchConfig.java
@@ -5,7 +5,9 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ElasticsearchConfig extends ElasticsearchProperties {
 
     @Bean
--- src/test/java/com/usian/test/IndexWriterTest.java
+++ src/test/java/com/usian/test/IndexWriterTest.java
@@ -1,13 +1,22 @@
 package com.usian.test;
 
 import com.usian.ESApp;
-import org.elasticsearch.client.RestClient;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexResponse;
+import org.elasticsearch.client.IndicesClient;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = {ESApp.class})
@@ -16,14 +25,64 @@ public class IndexWriterTest {
     @Autowired
     private RestHighLevelClient restHighLevelClient ;
 
+    //创建索引库
     @Test
-    public void testCrateIndex(){
-
+    public void testCrateIndex() throws IOException {
+        //创建“创建索引请求”对象，并设置索引名称
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest("java1906");
+        //设置索引参数
+        createIndexRequest.settings(Settings.builder().put("number_of_shards",1).put("number_of_replicas",0));
+        createIndexRequest.mapping("course","{\n" +
+                "  \"_source\": {\n" +
+                "    \"excludes\":[\"description\"]\n" +
+                "  }, \n" +
+                " \t\"properties\": {\n" +
+                "      \"name\": {\n" +
+                "          \"type\": \"text\",\n" +
+                "          \"analyzer\":\"ik_max_word\",\n" +
+                "          \"search_analyzer\":\"ik_smart\"\n" +
+                "      },\n" +
+                "      \"description\": {\n" +
+                "          \"type\": \"text\",\n" +
+                "          \"analyzer\":\"ik_max_word\",\n" +
+                "          \"search_analyzer\":\"ik_smart\"\n" +
+                "       },\n" +
+                "       \"studymodel\": {\n" +
+                "          \"type\": \"keyword\"\n" +
+                "       },\n" +
+                "       \"price\": {\n" +
+                "          \"type\": \"float\"\n" +
+                "       },\n" +
+                "       \"pic\":{\n" +
+                "\t\t   \"type\":\"text\",\n" +
+                "\t\t   \"index\":false\n" +
+                "\t    },\n" +
+                "       \"timestamp\": {\n" +
+                "      \t\t\"type\":   \"date\",\n" +
+                "      \t\t\"format\": \"yyyy-MM-dd HH:mm:ss||yyyy-MM-dd\"\n" +
+                "    \t }\n" +
+                "  }\n" +
+                "}", XContentType.JSON);
+        //创建索引操作客户端
+        IndicesClient indices = restHighLevelClient.indices();
+        //创建响应对象
+        CreateIndexResponse createIndexResponse = indices.create(createIndexRequest, RequestOptions.DEFAULT);
+        //得到响应结果
+        boolean acknowledged = createIndexResponse.isAcknowledged();
+        System.out.println("acknowledged:"+acknowledged);
     }
 
-
+    //删除索引库
     @Test
-    public void testDeleteIndex(){
-
+    public void testDeleteIndex() throws IOException {
+        //创建“删除索引请求”对象
+        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("java1906");
+        //创建索引操作客户端
+        IndicesClient indices = restHighLevelClient.indices();
+        //创建响应对象
+        DeleteIndexResponse deleteIndexResponse = indices.delete(deleteIndexRequest, RequestOptions.DEFAULT);
+        //得到响应结果
+        boolean acknowledged = deleteIndexResponse.isAcknowledged();
+        System.out.println("acknowledged:"+acknowledged);
     }
 }
